2e9dec7208fe036c07fcfe11038935f0685e16eb
Rhys Streefland <rhys@streefland.co.uk>
2023-08-12 17:53:48 +0100

    refactor: extract price cap to variable

--- kindle/local/fetch-dashboard.sh
+++ kindle/local/fetch-dashboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # Fetch a new dashboard image, make sure to output it to "$1".
 # For example:
-"$(dirname "$0")/../xh" -d -q -o "$1" get "https://REPLACE_ME_WITH_URL?token=REPLACE_ME_WITH_TOKEN&batteryPercentage=$(lipc-get-prop -- com.lab126.powerd battLevel)"
+"$(dirname "$0")/../xh" -d -q -o "$1" get "https://REPLACE_ME_WITH_YOUR_URL?token=REPLACE_ME_WITH_YOUR_TOKEN&batteryPercentage=$(lipc-get-prop -- com.lab126.powerd battLevel)"
